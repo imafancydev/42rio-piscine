01fdcee34b3f83e352a19c13b9a7383b4b633062
Lucas Leão <lucasaugustoleon@gmail.com>
2024-11-18 00:05:50 -0300

    C piscine C00 | OK 88%

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laugusto <laugusto@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/17 23:46:10 by laugusto          #+#    #+#             */
+/*   Updated: 2024/11/18 00:05:15 by laugusto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_putstr(char *str)
+{
+	int	i;
+
+	i = -1;
+	if (!str)
+		return ('\0');
+	while (str[++i])
+	{
+		write(1, &str[i], 1);
+	}
+	return (i);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putstr("-2147483648");
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb >= 0 && nb < 10)
+		ft_putchar(nb + '0');
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
